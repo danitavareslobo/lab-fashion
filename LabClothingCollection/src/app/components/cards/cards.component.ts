78d738be9dc8808288a3607aced6feba39d600f9
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2023-04-02 13:52:58 -0300

    Criação Cards Dashboard
    Criação Cards Dashboard

--- LabClothingCollection/src/app/components/cards/cards.component.ts
+++ LabClothingCollection/src/app/components/cards/cards.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
+  constructor() { }
 
+  @Input() public titulo!: string;
+  @Input() public valor!: number;
+  @Input() public info: boolean = false;
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 }

1f5a052e417dbf444b3490f6b7e3e48e0278330e
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2023-04-02 23:31:16 -0300

    Criação Lista de Coleções
    Criação Lista de Coleções

--- LabClothingCollection/src/app/pages/collections/collections.component.ts
+++ LabClothingCollection/src/app/pages/collections/collections.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import {CollectionServicesService} from '../../services/collection-services.service'
 
 @Component({
   selector: 'app-collections',
   templateUrl: './collections.component.html',
   styleUrls: ['./collections.component.scss']
 })
-export class CollectionsComponent {
+export class CollectionsComponent implements OnInit {
+  public collections: any[] = [];
 
+  constructor(private _router: Router,
+              private _api: HttpClient,
+              private _service: CollectionServicesService) {
+
+  }
+
+  ngOnInit(): void {
+    this._service.getCollections().then((response: any)=> {
+      console.log(response);
+      this.collections = response
+    });
+   
+  }
+
+  onCreate() {
+    this._router.navigate(['/private/register-collection'])
+  }
+
+  onEdit(row: any): void {
+    this._router.navigate(['/private/edit-collection/'+ row.id])
+  }
 }

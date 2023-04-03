d51972014b0ec33830fa94e761b13a37c7cb10e9
Daniele Tavares Lobo <danitavares.dev@gmail.com>
2023-04-02 23:37:30 -0300

    Criação Lista de Modelos
    Criação Lista de Modelos

--- LabClothingCollection/src/app/pages/models/models.component.ts
+++ LabClothingCollection/src/app/pages/models/models.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-models',
   templateUrl: './models.component.html',
   styleUrls: ['./models.component.scss']
 })
-export class ModelsComponent {
+export class ModelsComponent implements OnInit {
+  public models: any[] = [];
 
+  constructor(private _router: Router,
+              private _api: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this._api.get("http://localhost:3000/modelos").subscribe((response: any) => {
+      this.models = response || [];
+    });
+  }
+
+onCreateModel() {
+ this._router.navigate(['/private/register-model'])
+}
+
+onEdit(row: any): void {
+  this._router.navigate(['/private/edit-model/'+ row.id])
+}
 }
